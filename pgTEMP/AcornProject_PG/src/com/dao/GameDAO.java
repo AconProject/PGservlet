1c1852e5b8d819fc5d1157d73fcaaa7318d917a4
HeathChang <jannhyunsoo@gmail.com>
2021-04-28 18:12:36 +0900

    pgTemp

---
+++ pgTEMP/AcornProject_PG/src/com/dao/GameDAO.java
@@ -0,0 +1,24 @@
+package com.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.dto.GameDTO;
+
+public class GameDAO {
+	public List<GameDTO> newGameListSelect(SqlSession session) {
+		List<GameDTO> list = session.selectList("GameMapper.newGameListSelect");
+		return list;
+	}
+	
+	public List<GameDTO> recommendGameListSelect(SqlSession session) {
+		List<GameDTO> list = session.selectList("GameMapper.recommendGameListSelect");
+		return list;
+	}
+	
+	public List<GameDTO> tagGameListSelect(SqlSession session, String[] gameTags) {
+		List<GameDTO> list = session.selectList("GameMapper.tagGameListSelect");
+		return list;
+	}
+}
