bf5a611983c3b4ddc438e6426c2692746b26753d
CheeseB <2489ckckck@naver.com>
2021-05-11 18:01:00 +0900

    js 클릭이벤트 등록

--- pgTEMP/AcornProject_PG/WebContent/JS/main.js
+++ pgTEMP/AcornProject_PG/WebContent/JS/main.js
@@ -1,37 +1,49 @@
-window.onload = function() {
-	console.log('js test');
+window.onload = function(){
 	document.getElementById('newGame').addEventListener('click', getNewGame, false);
 	document.getElementById('recommendedGame').addEventListener('click', getRecommendedGame, false);
 	const tags = document.getElementsByName('tag');
-	tags.forEach((e) => {
-		e.addEventListener('click', function(){
-			console.log('checked');
-		}, false);
+	tags.forEach((tag) => {
+		tag.addEventListener('click', getCheckboxValue, false);
 	});
+	document.getElementById('recommendedPost').addEventListener('click', getRecommendedPost, false);
+	document.getElementById('mostViewPost').addEventListener('click', getMostViewPost, false);
+	document.getElementById('recommendedQnA').addEventListener('click', getRecommendedQnA, false);
+	document.getElementById('mostViewQnA').addEventListener('click', getMostViewQnA, false);
 };
 
-function getNewGame() {
-	// 임시 코드
+function getNewGame(){
 	console.log('click1');
 }
 
 function getRecommendedGame(){
-	// 임시 코드
 	console.log('click2');
 }
 
-// function getCheckboxValue() {
-// 	console.log('이벤트 동작 확인');
-// 	const query = 'input[name="tag"]:checked';
-// 	const selectedEls = 
-// 		document.querySelectorAll(query);
+function getCheckboxValue(){
+	const query = 'input[name="tag"]:checked';
+	const selectedEls = 
+		document.querySelectorAll(query);
 	
-// 	// 임시 코드
-// 	let result = '';
-// 	selectedEls.forEach((el) => {
-// 		result += el.value + ' ';
-// 	});
+	let result = '';
+	selectedEls.forEach((el) => {
+		result += el.value + ' ';
+	});
 	
-// 	// 출력
-// 	document.getElementById('result').innerText = '선택된 태그: ' + result;
-// }
+	console.log(result);
+}
+
+function getRecommendedPost(){
+	console.log('click3');
+}
+
+function getMostViewPost(){
+	console.log('click4');
+}
+
+function getRecommendedQnA(){
+	console.log('click5');
+}
+
+function getMostViewQnA(){
+	console.log('click6');
+}
