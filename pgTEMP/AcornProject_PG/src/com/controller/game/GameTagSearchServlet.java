b58ce4302394ed93e74efbacf521249b1d396053
ambosing <ambosing_@naver.com>
2021-05-15 22:53:42 +0900

    board servlet

--- pgTEMP/AcornProject_PG/src/com/controller/game/GameTagSearchServlet.java
+++ pgTEMP/AcornProject_PG/src/com/controller/game/GameTagSearchServlet.java
@@ -26,7 +26,6 @@ public class GameTagSearchServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String
 	}
 
 	/**
