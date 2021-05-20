626143946c781481fcd62d0ec9eb72d1cac04eaf
HeathChang <jannhyunsoo@gmail.com>
2021-05-20 16:29:56 +0900

    210520_gameDetailServlet_fin

--- PlayGround_1/AcornProject_PG/src/com/dao/GameDAO.java
+++ PlayGround_1/AcornProject_PG/src/com/dao/GameDAO.java
@@ -12,30 +12,34 @@ public class GameDAO {
 		List<GameDTO> list = session.selectList("GameMapper.newGameListSelect");
 		return list;
 	}
-	
+
 	public List<GameDTO> recommendGameListSelect(SqlSession session, int limit) {
 		List<GameDTO> list = session.selectList("GameMapper.recommendGameListSelect", limit);
 		return list;
 	}
-	
+
 	public List<GameDTO> tagGameListSelect(SqlSession session) {
 		List<GameDTO> list = session.selectList("GameMapper.tagGameListSelect");
 		return list;
 	}
 
 	public GameDTO detailGameSelect(SqlSession session, String gameNo) {
-		GameDTO dto = 
-				   session.selectOne("GameMapper.detailGameSelect", gameNo);
-		   return dto;
+		GameDTO dto = session.selectOne("GameMapper.detailGameSelect", gameNo);
+		return dto;
 	}
-	
+
 	public List<GameDTO> recommendUserTagListSelect(SqlSession session, String mbrId) {
 		List<GameDTO> list = session.selectList("GameMapper.recommendUserTagListSelect", mbrId);
 		return list;
 	}
-	
+
 	public List<GameDTO> tagGameListSelect(SqlSession session, List<String> listTags) {
 		List<GameDTO> list = session.selectList("GameMapper.tagGameListSelect", listTags);
+		return list;
+	}
+
+	public List<GameDTO> relatedGameList(SqlSession session, String gameCategory) {
+		List<GameDTO> list = session.selectList("GameMapper.relatedGameList", gameCategory);
 		return list;
 	}
 
