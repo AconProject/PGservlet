4c4bc99e556e28efaf691da881e8a3e98ba2b043
CheeseB <2489ckckck@naver.com>
2021-05-25 18:06:33 +0900

    게시판 리스트 데이터 출력

--- PlayGround_1/AcornProject_PG/WebContent/JS/boardList.js
+++ PlayGround_1/AcornProject_PG/WebContent/JS/boardList.js
@@ -7,16 +7,43 @@ window.onload = function(){
 	getBoardList();
 };
 
+/* JSP에 새로운 태그 및 컨텐츠 삽입 */
+function insertElement(childTag, parentId, content, attr, attrVal){
+	let newEle = document.createElement(childTag);
+	if (attr && attrVal){
+		newEle.setAttribute(attr, attrVal);
+	}
+	newEle.innerHTML = content;
+	let parentEle = document.getElementById(parentId);
+	parentEle.appendChild(newEle);
+}
+
 /* 게시판 글 목록 불러오기 (첫 로딩)*/
 function getBoardList(){
-	fetch('../BoardListServlet?boardKind=boardList&&boardCategory=common')
+	fetch('../BoardListServlet?boardKind=boardList')
 		.then(res => res.json())
 		.then(data => {
-			console.log(data);
+			jsonParserForBoard(data);
 		})
 		.catch(err => {
 			console.log(err);
 		});
+}
+
+/* 게시판 데이터 파싱 후 출력 */
+function jsonParserForBoard(data){
+	for (let i=0; i<data.length; i++){
+		let jsonObj = JSON.parse(data[i]);
+		console.log(jsonObj);
+		insertElement('tr', 'boardList', '', 'id', 'board'+i);
+		insertElement('td', 'board'+i, jsonObj.boardCategory);
+		insertElement('td', 'board'+i, '<a href="../BoardDetailServlet?boardId='
+			+ jsonObj.boardId + '">' + jsonObj.boardName + '</a>');
+		insertElement('td', 'board'+i, jsonObj.mbrName);
+		insertElement('td', 'board'+i, jsonObj.boardDate);
+		insertElement('td', 'board'+i, jsonObj.boardCount);
+		insertElement('td', 'board'+i, jsonObj.boardLiked);
+	}
 }
 
 /* 게시판 검색 결과 불러오기 */
