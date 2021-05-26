5d8c3966fdb42099bc9c0fe7034bbb9b97328cb1
CheeseB <2489ckckck@naver.com>
2021-05-26 16:39:14 +0900

    카테고리 필터링 기능 추가

--- PlayGround_1/AcornProject_PG/WebContent/JS/boardList.js
+++ PlayGround_1/AcornProject_PG/WebContent/JS/boardList.js
@@ -4,6 +4,9 @@ window.onload = function(){
 		location.href = '../Board/writeBoard.jsp';
 	}, false);
 
+	// 카테고리 선택 이벤트 등록
+	document.getElementById('boardCategory').addEventListener('change', getChangedBoardList, false);
+
 	getBoardList();
 };
 
@@ -18,6 +21,29 @@ function insertElement(childTag, parentId, content, attr, attrVal){
 	parentEle.appendChild(newEle);
 }
 
+/* 모든 요소 삭제 (데이터 갱신 시 기존 데이터 삭제 위함) */
+function removeAllElements(query){
+	let removeEles = document.querySelectorAll(query);
+	for (let i=0; i<removeEles.length; i++){
+		removeEles[i].parentNode.removeChild(removeEles[i]);
+	}
+}
+
+/* 게시판 데이터 파싱 후 출력 */
+function jsonParserForBoard(data){
+	for (let i=0; i<data.length; i++){
+		let jsonObj = JSON.parse(data[i]);
+		insertElement('tr', 'boardList', '', 'id', 'board'+i);
+		insertElement('td', 'board'+i, jsonObj.boardCategory);
+		insertElement('td', 'board'+i, '<a href="../BoardDetailServlet?boardId='
+			+ jsonObj.boardId + '">' + jsonObj.boardName + '</a>');
+		insertElement('td', 'board'+i, jsonObj.mbrName);
+		insertElement('td', 'board'+i, jsonObj.boardDate);
+		insertElement('td', 'board'+i, jsonObj.boardCount);
+		insertElement('td', 'board'+i, jsonObj.boardLiked);
+	}
+}
+
 /* 게시판 글 목록 불러오기 (첫 로딩)*/
 function getBoardList(){
 	fetch('../BoardListServlet?boardKind=boardList')
@@ -30,20 +56,20 @@ function getBoardList(){
 		});
 }
 
-/* 게시판 데이터 파싱 후 출력 */
-function jsonParserForBoard(data){
-	for (let i=0; i<data.length; i++){
-		let jsonObj = JSON.parse(data[i]);
-		console.log(jsonObj);
-		insertElement('tr', 'boardList', '', 'id', 'board'+i);
-		insertElement('td', 'board'+i, jsonObj.boardCategory);
-		insertElement('td', 'board'+i, '<a href="../BoardDetailServlet?boardId='
-			+ jsonObj.boardId + '">' + jsonObj.boardName + '</a>');
-		insertElement('td', 'board'+i, jsonObj.mbrName);
-		insertElement('td', 'board'+i, jsonObj.boardDate);
-		insertElement('td', 'board'+i, jsonObj.boardCount);
-		insertElement('td', 'board'+i, jsonObj.boardLiked);
-	}
+/* 게시판 글 목록 불러오기 (카테고리 선택 시)*/
+function getChangedBoardList(){
+	let target = document.getElementById('boardCategory');
+	let selectedCategory = target.options[target.selectedIndex].value;
+
+	fetch('../BoardListServlet?boardKind=boardList&&boardCategory='+selectedCategory)
+		.then(res => res.json())
+		.then(data => {
+			removeAllElements('tr[id^="board"]');
+			jsonParserForBoard(data);
+		})
+		.catch(err => {
+			console.log(err);
+		});
 }
 
 /* 게시판 검색 결과 불러오기 */
